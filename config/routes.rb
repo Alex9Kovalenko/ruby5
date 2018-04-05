714030e317ed09f306abbb8df12a905e104f0c9a
Uncle <alex.the.kovalenko@gmail.com>
2018-04-05 03:24:12 +0300

    Changed the main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'main/index'
+
   resources :authors
   resources :areas
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "main#show", page: "index"
 end
